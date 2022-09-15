becb2a0154c692c9e8dea996829351e717cb1629
Aruming <0430kar@naver.com>
2022-09-15 09:11:02 +0900

    Lv1 약수의 합

--- Solution.java
+++ Solution.java
@@ -1,15 +1,10 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        while(n != 0){
-            answer += n%10;
-            n = n/10;
+        for(int i=1;i<=n;i++){
+            if(n%i==0)
+                answer+=i;
         }
-
-
         return answer;
     }
 }
