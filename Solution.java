cbccfd72833068fd32a031c14c61083e8b2f2f8c
Aruming <0430kar@naver.com>
2022-09-15 09:14:10 +0900

    Lv1 평균 구하기

--- Solution.java
+++ Solution.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0)
-                answer+=i;
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i=0;i<arr.length;i++){
+            answer+=arr[i];
         }
+        answer = answer/arr.length;
         return answer;
     }
 }
