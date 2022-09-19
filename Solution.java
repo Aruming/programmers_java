aba21e90846cab9b94efa86e27606d2c239c6cd5
Aruming <0430kar@naver.com>
2022-09-19 09:15:25 +0900

    Lv1 하샤드 수

--- Solution.java
+++ Solution.java
@@ -1,7 +1,18 @@
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        answer = Integer.parseInt(s);
+    public boolean solution(int x) {
+        boolean answer = true;
+        int num = 0;
+        int tmp = x;
+
+        while(tmp!=0){
+            num += tmp%10;
+            tmp /= 10;
+        }
+
+        if(x%num != 0){
+            answer = false;
+        }
+
         return answer;
     }
 }
