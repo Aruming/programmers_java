ed41d8306319b89e09779c77b8c720b1db7a78d4
Aruming <0430kar@naver.com>
2022-09-19 09:06:29 +0900

    Lv1 문자열을 정수로 바꾸기

--- Solution.java
+++ Solution.java
@@ -1,27 +1,7 @@
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        int pnum = 0;
-        int ynum = 0;
-
-        s = s.toLowerCase();
-
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='p'){
-                pnum++;
-            }
-            if(s.charAt(i)=='y'){
-                ynum++;
-            }
-        }
-
-        if(pnum==ynum){
-            answer = true;
-        }
-        else{
-            answer = false;
-        }
-
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
         return answer;
     }
 }
