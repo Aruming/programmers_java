a42789d436692a8aa042bc991d67b0ba9b792957
Aruming <0430kar@naver.com>
2022-09-23 09:17:38 +0900

    LV1 콜라츠 추측

--- Solution.java
+++ Solution.java
@@ -1,11 +1,29 @@
 class Solution {
-    public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        long num = x;
+    public int solution(int num) {
+        int answer = 0;
+        int cnt = 0;
+        long n = num;
 
-        for(int i=0;i<n;i++){
-            answer[i] = num;
-            num += x;
+        if(n==1){
+            answer = 0;
+        }
+        else{
+            while(n!=1){
+                if(cnt>=500){
+                    cnt = -1;
+                    break;
+                }
+
+                if(n%2==0){
+                    n/=2;
+                    cnt++;
+                }
+                else{
+                    n = (n*3)+1;
+                    cnt++;
+                }
+            }
+            answer = cnt;
         }
 
         return answer;
