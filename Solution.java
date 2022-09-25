fce39f4f87de084c774d59ff2f4274df5c78a8c8
Aruming <0430kar@naver.com>
2022-09-25 18:42:15 +0900

    Lv1 나누어 떨어지는 숫자 배열

--- Solution.java
+++ Solution.java
@@ -1,14 +1,28 @@
+import java.util.Arrays;
 class Solution {
-    public String solution(String phone_number) {
-        String answer = "";
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        int cnt = 0;
 
-        for(int i=0;i<phone_number.length();i++){
-            if(i<phone_number.length()-4){
-                answer += "*";
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%divisor == 0)
+                cnt++;
+        }
+
+        if(cnt==0){
+            answer = new int[1];
+            answer[0] = -1;
+        }
+        else{
+            answer = new int[cnt];
+            cnt = 0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]%divisor == 0){
+                    answer[cnt]=arr[i];
+                    cnt++;
+                }
             }
-            else{
-                answer +=phone_number.charAt(i);
-            }
+            Arrays.sort(answer);
         }
 
         return answer;
