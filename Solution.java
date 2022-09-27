e9c3706887c95caca5073bf1da51e70ced6a7fc2
Aruming <0430kar@naver.com>
2022-09-27 09:01:42 +0900

    Lv1 음양 더하기

--- Solution.java
+++ Solution.java
@@ -1,28 +1,14 @@
 class Solution {
-    public int[] solution(int[] arr) {
-        int[] answer = {};
-        int num = arr[0];
-        int cnt = 0;
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
 
-        if (arr.length > 1) {
-            answer = new int[arr.length - 1];
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] < num) {
-                    num = arr[i];
-                }
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]==true){
+                answer += absolutes[i];
             }
-
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == num)
-                    continue;
-                else {
-                    answer[cnt] = arr[i];
-                    cnt++;
-                }
+            else{
+                answer -= absolutes[i];
             }
-        } else {
-            answer = new int[1];
-            answer[0] = -1;
         }
 
         return answer;
