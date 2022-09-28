6806156d5b6eb15b48a572bcd5ab962075e46a81
Aruming <0430kar@naver.com>
2022-09-28 09:38:40 +0900

    Lv1 내적

--- Solution.java
+++ Solution.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int solution(int[] numbers) {
-        int answer = 45;
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
 
-        for(int i=0;i<numbers.length;i++){
-            answer -= numbers[i];
+        for(int i=0;i<a.length;i++){
+            answer += a[i]*b[i];
         }
 
         return answer;
