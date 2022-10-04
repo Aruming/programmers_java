c372610344c16ac67c9a51e29da12496da1dbb88
Aruming <0430kar@naver.com>
2022-10-04 17:44:43 +0900

    Lv1 최대공약수와 최소공배수

--- Solution.java
+++ Solution.java
@@ -1,16 +1,27 @@
-import java.util.Scanner;
-
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    public int[] solution(int n, int m) {
+        int[] answer = {1, 1};
 
-        for(int i=0;i<b;i++){
-            for(int j=0;j<a;j++){
-                System.out.print("*");
+        int num = n>m?n:m;
+        int i=2;
+
+        while(i<num){
+            if(n%i==0 && m%i==0){
+                answer[0] *= i;
+                answer[1] *= i;
+
+                n /= i;
+                m /= i;
+
+                i = 2;
             }
-            System.out.println("");
+            else{
+                i++;
+            }
         }
+
+        answer[1] *= n*m;
+
+        return answer;
     }
 }
