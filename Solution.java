f86b8a01ef880e8eb2cb716fe427d1c06ed5d419
Aruming <0430kar@naver.com>
2022-10-07 09:23:34 +0900

    Lv1 3진법 뒤집기

--- Solution.java
+++ Solution.java
@@ -1,25 +1,30 @@
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        String[] arr = s.split("");
-        int n=0;
+    public int solution(int n) {
+        int answer = 0;
+        int size = 0;
+        int tmp = n;
 
-        for(int i=0;i<arr.length;i++){
+        while(tmp>0){
+            tmp /= 3;
+            size++;
+        }
 
-            if(arr[i].equals(" ")){
-                answer += " ";
-                n=0;
+        int[] ternary = new int[size];
+        for(int i=0;i<size;i++){
+            if(n==1){
+                ternary[i] = 1;
             }
             else{
-                if(n==0 || n%2==0){
-                    answer += arr[i].toUpperCase();
-                }
-                else{
-                    answer += arr[i].toLowerCase();
-                }
-                n++;
+                ternary[i] = n%3;
+                n /= 3;
             }
+            System.out.println(ternary[i]);
+        }
 
+        int pow = 0;
+        for(int i=size-1;i>=0;i--){
+            answer += Math.pow(3, pow)*ternary[i];
+            pow++;
         }
 
         return answer;
