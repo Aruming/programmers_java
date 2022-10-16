ecfa3f1f01776bf3c156db3f06729d3a70e52996
Aruming <0430kar@naver.com>
2022-10-16 16:48:43 +0900

    Lv1 두 개 뽑아서 더하기

--- Solution.java
+++ Solution.java
@@ -1,14 +1,24 @@
+import java.util.Arrays;
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        String[] num = {"zero", "one", "two", "three", "four", "five",
-                "six", "seven", "eight", "nine"};
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
 
-        for(int i=0;i<10;i++){
-            s = s.replace(num[i], Integer.toString(i));
+        int[] arr = new int[numbers.length * (numbers.length-1)];
+        int index = 0;
+
+        for(int i=0;i<numbers.length;i++){
+            for(int j = 0;j<numbers.length;j++){
+                if(i!=j){
+                    arr[index] = numbers[i]+numbers[j];
+                    index++;
+                }
+            }
         }
 
-        answer = Integer.parseInt(s);
+        int[] newArr = Arrays.stream(arr).distinct().toArray();
+        Arrays.sort(newArr);
+
+        answer = newArr;
 
         return answer;
     }
