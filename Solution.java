700cb3b06af29a17da01d5c52b3aab10ef55def4
Aruming <0430kar@naver.com>
2022-11-22 15:50:27 +0900

    Lv1 모의고사

--- Solution.java
+++ Solution.java
@@ -1,18 +1,28 @@
-import java.util.Arrays;
 class Solution {
-    public String solution(String s) {
-        String answer = "";
+    public int[] solution(int[] answers) {
+        int[] score = new int[3];
 
-        String[] nums = s.split(" ");
-        int[] nums_ = new int[nums.length];
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        for (int i=0;i<nums.length;i++){
-            nums_[i] = Integer.parseInt(nums[i]);
+        for(int i=0;i<answers.length;i++){
+            if(answers[i] == p1[i%p1.length]) score[0]++;
+            if(answers[i] == p2[i%p2.length]) score[1]++;
+            if(answers[i] == p3[i%p3.length]) score[2]++;
         }
 
-        Arrays.sort(nums_);
+        int max = Math.max(Math.max(score[0], score[1]), score[2]);
+        int len = 0;
+        for(int i=0;i< score.length;i++){
+            if(score[i] == max) len++;
+        }
 
-        answer += nums_[0] + " " + nums_[nums_.length-1];
+        int[] answer = new int[len];
+        int idx = 0;
+        for(int i=0;i<score.length;i++){
+            if(score[i] == max) answer[idx++] = i+1;
+        }
 
         return answer;
     }
