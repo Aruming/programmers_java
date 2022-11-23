0fc991a7fba8af58073c6f33f5ec5ce83a766389
Aruming <0430kar@naver.com>
2022-11-23 17:31:43 +0900

    Lv2 JadenCase 문자열 만들기

--- Solution.java
+++ Solution.java
@@ -1,29 +1,20 @@
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] score = new int[3];
+    public String solution(String s) {
+        String answer = "";
+        String[] words = s.split("");
 
-        int[] p1 = {1, 2, 3, 4, 5};
-        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        for(int i=0;i< words.length;i++){
+            words[0] = words[0].toUpperCase();
 
-        for(int i=0;i<answers.length;i++){
-            if(answers[i] == p1[i%p1.length]) score[0]++;
-            if(answers[i] == p2[i%p2.length]) score[1]++;
-            if(answers[i] == p3[i%p3.length]) score[2]++;
+            if(i>0){
+                if(words[i-1].equals(" ")){
+                    words[i] = words[i].toUpperCase();
+                }else{
+                    words[i] = words[i].toLowerCase();
+                }
+            }
+            answer += words[i];
         }
-
-        int max = Math.max(Math.max(score[0], score[1]), score[2]);
-        int len = 0;
-        for(int i=0;i< score.length;i++){
-            if(score[i] == max) len++;
-        }
-
-        int[] answer = new int[len];
-        int idx = 0;
-        for(int i=0;i<score.length;i++){
-            if(score[i] == max) answer[idx++] = i+1;
-        }
-
         return answer;
     }
 }
