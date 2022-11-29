31637797c5262fb66aa373042d201adb7b707823
Aruming <0430kar@naver.com>
2022-11-29 17:43:11 +0900

    Lv2 올바른 괄호 - Stack 사용 x

--- Solution.java
+++ Solution.java
@@ -1,24 +1,24 @@
-import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         boolean answer = false;
 
-        Stack<Character> stack = new Stack<>();
+        int cnt = 0;
 
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                stack.push(s.charAt(i));
+                cnt++;
             }else{
-                if(stack.isEmpty()){
+                if(cnt==0){
                     return false;
                 }
-                stack.pop();
+                cnt--;
             }
         }
 
-        if(stack.isEmpty()){
+        if(cnt==0){
             answer = true;
         }
+
         return answer;
     }
 }
